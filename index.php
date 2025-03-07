dc50cc61db25549474865ffddd2018b4e5b1c601
Maycon Do Carmo <maycongusmao.up@gmail.com>
2025-03-07 01:57:57 -0300

    :wrench: chore: Importando templates

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php 
-  include_once("helpers/url.php");
+  include_once("templates/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blog Sem Money</title>
-  <link rel="stylesheet" href="<?php $BASE_URL?>css/style.css">
-</head>
-<body>
-  
-</body>
-</html>
+
+<?php 
+  include_once("templates/footer.php")
+?>
