d32f653b4db997ab85a986eaf1fd638eb33e635b
Maycon Do Carmo <maycongusmao.up@gmail.com>
2025-03-07 01:55:17 -0300

    :building_construction: feat: Adicionando estrutura inicial do template

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <p>Todos os direitos reservados a Sem Money &copy; 2025</p>
+</footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,30 @@
+<?php 
+  include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog Sem Money</title>
+  <link rel="stylesheet" href="<?php $BASE_URL?>css/style.css">
+  <!--GOOGLE FONTS-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <a href="<?php $BASE_URL ?>" id="logo">
+      <img src="<?php $BASE_URL ?>img/logo.svg" alt="Blog Sem Money">
+    </a>
+    <nav>
+      <ul>
+        <li><a href="<?php $BASE_URL ?>">Home</a></li>
+        <li><a href="#">Categorias</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="<?php $BASE_URL ?>">contact.php</a></li>
+      </ul>
+    </nav>
+  </header>
+  <h1>header template</h1>
